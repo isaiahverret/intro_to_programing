b1f5507a573a18effc1bf45a22c7499e9bef63f1
isaiahverret <verretisaiah@yahoo.com>
2016-08-18 12:39:22 -0500

    examples
    exercise

---
+++ Project2/Source.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+int main()
+{
+	//Example 1
+	
+	/*
+		int num = 2 / 3 + 3 / 5;
+	printf("%i", num);
+	system("pause");
+		return 0;
+*/
+    //Example 2
+
+	/*int num = 2 % 3 + 3 % 5;
+	printf("%i", num);
+	system("pause");
+	return 0;*/
+
+	//Example 3
+
+	/*int num = 23 % 15 % -2;
+		printf("%i", num);
+	system("pause");
+	return 0;*/
+
+	//Example 4
+
+	//int num = 25 * 1 / 2;
+	//printf("%i", num);
+	//system("pause");
+	//return 0;
+	//
+
+	//Example 5
+
+	//int num = 25 * 1.0 / 2;
+	//printf("%i", num);
+	//system("pause");
+	//return 0;
+
+	//Example 6
+
+	/*int num = 25 * (1 / 2);
+	printf("%i", num);
+	system("pause");
+	return 0;*/
+
+	//Example 7
+
+	/*int num = 235 / 8 + 7;
+	printf("%i", num);
+	system("pause");
+	return 0;*/
+
+	//Example 8
+
+	/*int num = 235 / 8.0 + 7;
+	printf("%i", num);
+	system("pause");
+	return 0; 
+*/
+
+	//Example 9
+
+	/*int  num = ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2;
+	printf("%i", num);
+	system("pause");
+	return 0;*/
+
+
+	//Example 10
+
+	//Avarge hight;
+	//system("pause");
+	
+
+	//Example 11
+	//a hight tax rate
+		//system("pause");
+		
+
+	//Example 12
+	// a low tax rate 
+	//System("pause")
+
+	//Example 13
+	// 0$
+	//System("pause")
+
+	//Example 14
+	//int a=121
+	
+
+	//Example 15
+	//Float b=9.4f
+	
+	//Example 16
+	//char c = 'r'
+	
+
+	//Example 17
+	//int d=100000
+
+   
+
+    //Example 18
+    //bool e= false
+    
+	//system("pause");
+     // returne o ;
+
+//Example 19 
+
+
+
+
+
+
+
+
+
+
+
+
+}
