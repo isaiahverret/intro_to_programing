3fac89930f7c21aa0562f34e99213980c14619f1
isaiahverret <verretisaiah@yahoo.com>
2016-08-18 15:56:36 -0500

    exercise variable

--- Exercise-Variables/Source.cpp
+++ Exercise-Variables/Source.cpp
@@ -2,130 +2,186 @@
 int main()
 {
 	//Example 1
+	//A
 
-	/*
-	int num = 2 / 3 + 3 / 5;
+	
+	/*int num = 2 / 3 + 3 / 5;
 	printf("%i", num);
 	system("pause");
 	return 0;
-	*/
-	//Example 2
+
+	answer 0*/
+	
+	//B
 
 	/*int num = 2 % 3 + 3 % 5;
 	printf("%i", num);
 	system("pause");
-	return 0;*/
+	return 0;
 
-	//Example 3
+	Answer 5*/
 
-	/*int num = 23 % 15 % -2;
-	printf("%i", num);
-	system("pause");
-	return 0;*/
+	//C
 
-	//Example 4
-
-	//int num = 25 * 1 / 2;
+	//int num = 23 % 15 % -2;
 	//printf("%i", num);
 	//system("pause");
 	//return 0;
-	//
 
-	//Example 5
+	//answer 0
+
+	//D
+
+	/*int num = 25 * 1 / 2;
+	printf("%i", num);
+	system("pause");
+	return 0;
+	
+	answer 12*/
+
+	//E
 
 	//int num = 25 * 1.0 / 2;
 	//printf("%i", num);
 	//system("pause");
 	//return 0;
 
-	//Example 6
+	//answer 12
+
+	//F
 
 	/*int num = 25 * (1 / 2);
 	printf("%i", num);
 	system("pause");
-	return 0;*/
-
-	//Example 7
-
-	/*int num = 235 / 8 + 7;
-	printf("%i", num);
-	system("pause");
-	return 0;*/
-
-	//Example 8
-
-	/*int num = 235 / 8.0 + 7;
-	printf("%i", num);
-	system("pause");
 	return 0;
-	*/
 
-	//Example 9
+	answer 0
+*/
+	//G
 
-	/*int  num = ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2;
-	printf("%i", num);
-	system("pause");
-	return 0;*/
+	//int num = 235 / 8 + 7;
+	//printf("%i", num);
+	//system("pause");
+	//return 0;
+
+	//answer 36
 
 
-	//Example 10
+	//H
 
-	//Avarge hight;
+	//int num = 235 / 8.0 + 7;
+	//printf("%i", num);
+	//system("pause");
+	//return 0;
+
+	//answer 36
+
+	//I
+
+	//int  num = ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2;
+	//printf("%i", num);
+	//system("pause");
+	//return 0;
+
+	/*answer 0*/
+
+	//Example 2
+	//A
+
+	//Avghight;
 	//system("pause");
 
 
-	//Example 11
-	//a hight tax rate
+	//B
+	//ahighttaxrate
 	//system("pause");
 
 
-	//Example 12
-	// a low tax rate 
+	//C
+	// alowtaxrate 
 	//System("pause")
 
-	//Example 13
+	//D
 	// 0$
 	//System("pause")
 
-	//Example 14
+	//Example  3
+	//A
 	//int a=121
 
 
-	//Example 15
+	//B
 	//Float b=9.4f
 
-	//Example 16
+	//C
 	//char c = 'r'
 
 
-	//Example 17
+	//D
 	//int d=100000
 
 
 
-	//Example 18
+	//E
 	//bool e= false
 
 	//system("pause");
 	// returne 0;
 
-	//Example 19 
+	//Example 4 
 
-int cama = 7;
-int camb = 9;
-int hold = 0;
+//int cama = 7;
+//int camb = 9;
+//int hold = 0;
+//
+// hold = camb ;
+//
+//camb = cama ;
+//
+// cama = hold ;
+//
+//printf("%i", cama);
+//printf("%i", camb);
+//
+//system("pause");
+//return 0;
 
- hold = camb ;
 
-camb = cama ;
+//Example 5
+//
+//int i = 0;
+//float f = 0;
+//char ch = 'a';
+//std::cin >> i >> ch >> f;
+//std::cout << "i: " << i << "\nch: " << ch << "\nf: " << f << std::endl;
+//
+//system("pause");
+//return 0;
 
- cama = hold ;
 
-printf("%i", cama);
-printf("%i", camb);
+//ANSWERS
+//a  i: 1  Ch:A f: 45
+//b  i: 1 ch:A  f:45
+//c  i: ch:A f:45
+//d    i:1 ch:9    f:45
+//e    i:1 ch:a f:0
+//f   i;1  ch:B f:0
 
-system("pause");
-return 0;
+
+//Example 6
+
+//int a = 2;
+//int b = 4;
+//int c = 6;
+//int d = 8;
+//int e = 10;
+//
+//printf("%i", (a + b + c + d + e) / 5);
+//
+//system("pause");
+//return 0;
+//
+//Answer 6 
 
 
 
