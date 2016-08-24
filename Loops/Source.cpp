8b5ed105a15517c5320617a94eaa06b2bc302540
isaiahverret@yahoo.com <belle5467>
2016-08-23 19:56:40 -0600

    loops

--- Loops/Source.cpp
+++ Loops/Source.cpp
@@ -6,64 +6,121 @@ int main()
 	//example 1
 	//a
 
-	//int cam = 100;
-	//for (int i = 100; i > -1; i--)
-	//{
-	//	std::cout << i << std::endl;
-	//}
-
-
-
-	//b
-
-
-
-	//int cam = 100;
-	//for (int i = 99; i > -1; i--)
-	//{
-	//	std::cout << i << std::endl;
-	//}
-
-
-
-
-	//c
-
-
 	/*int cam = 100;
-	for (int i = 100; i > 0; i--)
+	for (int i = 100; i > -1; i--)
 	{
 		std::cout << i << std::endl;
 	}
 */
 
 
-	//d
+//b
 
+
+
+//int cam = 100;
+//for (int i = 99; i > -1; i--)
+//{
+//	std::cout << i << std::endl;
+//}
+
+
+
+
+//c
+
+
+/*int cam = 100;
+for (int i = 100; i > 0; i--)
+{
+	std::cout << i << std::endl;
+}
+*/
+
+
+//d
+
+
+
+//int cam = 100;
+//for (int i = 0; i < 100; i++)
+//{
+//	if (i % 2 == 0)
+//	{
+//		continue;
+//	}
+//	std::cout << i << std::endl;
+//}
+
+
+
+//exampl  2
+
+//a
+	/*int cam = 100;
+	for (int i = 0; i < cam; i++)
+	{
+		if (i % 3 == 0)
+		{
+			 printf("fizz");
+		}
+		std::cout << i << std::endl;
+	}
+	*/
+
+	//b
 
 
 	//int cam = 100;
-	//for (int i = 0; i < 100; i++)
+	//for (int i = 0; i < cam; i++)
 	//{
-	//	if (i % 2 == 0)
+	//	if (i % 5 == 0 )
 	//	{
-	//		continue;
+	//		printf("buzz");
 	//	}
 	//	std::cout << i << std::endl;
 	//}
 
 
 
-	//exampl  2
+	//c
+
+	////int cam = 100;
+	////for (int i = 0; i < cam; i++)
+	////	
+	////{
+	////	if (i % 3 == 0)
+	////		if (i % 5 == 0)
+
+	////	{
+	////		printf("fizzbuzz");
+
+	////	}
+	////	std::cout << i << std::endl;
+	////}
+
+	//example 3
 
 
-	int cam = 100;
-	for (int i = 0; i < cam; i++)
-	{ 
-		if (i % 3 == 0)
-		{
-			printf("fizz")
-		}
+
+
+
+
+//example 4
+
+//	int i = 100;
+//	while (i = 100)
+//
+//		i > -1; i--;
+//
+//	{
+//std::cout << i << std::endl;
+//}
+
+	
+
+			
+		
  
 
 
