21d48db5e1b9695fcb64b0b7320fd98115542ce8
isaiahverret <verretisaiah@yahoo.com>
2016-08-25 17:45:16 -0500

    loops

--- Exercise-Conditional Statement/Source.cpp
+++ Exercise-Conditional Statement/Source.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main()
-//{
+{
 
 	//Example 1
 
@@ -41,81 +41,81 @@ int main()
 
 
 	////example 3
-	{
+	/*{*/
 
-	/*int cama[3];
-	std::cin >> cama[0] >> cama[1] >> cama[2];
-	int sum = cama[0] + cama[1] + cama[2];
-	if (sum = 0)
-	{
-		for (int i = 2; i >= 0; i--)
+		/*int cama[3];
+		std::cin >> cama[0] >> cama[1] >> cama[2];
+		int sum = cama[0] + cama[1] + cama[2];
+		if (sum = 0)
 		{
-			for (int j = 2; j >= 0; j--)
+			for (int i = 2; i >= 0; i--)
 			{
-				if (cama[j] < cama[j - 1])
+				for (int j = 2; j >= 0; j--)
 				{
-					int temp;
-					temp = cama[j];
-					cama[j] = cama[j - 1];
-					cama[j - 1] = temp;
+					if (cama[j] < cama[j - 1])
+					{
+						int temp;
+						temp = cama[j];
+						cama[j] = cama[j - 1];
+						cama[j - 1] = temp;
+					}
 				}
 			}
 		}
-	}
 
 
-	else if (sum < 0)
-	{
-		for (int i = 2; i >= 0; i--)
+		else if (sum < 0)
 		{
-			for (int j = 2; j >= 0; j--)
+			for (int i = 2; i >= 0; i--)
 			{
-				if (cama[j] > cama[j - 1])
+				for (int j = 2; j >= 0; j--)
 				{
-					int temp;
-					temp = cama[j];
-					cama[j] = cama[j - 1];
-					cama[j - 1] = temp;
+					if (cama[j] > cama[j - 1])
+					{
+						int temp;
+						temp = cama[j];
+						cama[j] = cama[j - 1];
+						cama[j - 1] = temp;
+					}
 				}
 			}
 		}
-	}
-	else
+		else
 
-		std::cout << cama[0] << std::endl << cama[1] << std::endl << cama[2] << std::endl;
+			std::cout << cama[0] << std::endl << cama[1] << std::endl << cama[2] << std::endl;
 
 
-*/
-
+	*/
 
 
 
 
 
 
-// Example 4
-	/*
-	int choice;
-	std::cin >> choice;
-	switch(choice)
-		{
-		case 1:
-			std::cout << "1";
-			break;
 
-		case 2:
-			std::cout << "2 or 3";
-			break;
+	// Example 4
+		/*
+		int choice;
+		std::cin >> choice;
+		switch(choice)
+			{
+			case 1:
+				std::cout << "1";
+				break;
 
-		case 3:
-			std::cout << "4";
-			break;
+			case 2:
+				std::cout << "2 or 3";
+				break;
 
-		case 4:
-			std::cout << "invalid";
-			}
-*/
-		
+			case 3:
+				std::cout << "4";
+				break;
+
+			case 4:
+				std::cout << "invalid";
+				}
+	*/
+
 
 
 	//Example 5
@@ -183,6 +183,86 @@ int main()
 
 
 //example 7
+
+
+
+
+		int month;
+
+
+		std::cout << "give number" << std::endl;
+
+		std::cin >> month;
+
+		switch (month)
+		{
+		case 1:
+		{
+			printf("january - 31 days \n");
+			break;
+		}
+		case 2:
+		{
+			printf("february - 29 days on leap year   28 on reg  \n");
+			break;
+		}
+		case 3:
+		{
+			printf("march - 31 days \n");
+			break;
+		}
+		case 4:
+		{
+			printf("april - 30 days \n");
+			break;
+		}
+		case 5:
+		{
+			printf("may - 31 days \n");
+			break;
+		}
+		case 6:
+		{
+			printf("june- 30 days \n");
+			break;
+		}
+		case 7:
+		{
+			printf("july - 31 days \n");
+			break;
+		}
+		case 8:
+		{
+			printf("august - 31 days \n");
+			break;
+		}
+		case 9:
+		{
+			printf("september - 30 days \n");
+			break;
+		}
+		case 10:
+		{
+			printf("october - 31 days \n");
+			break;
+		}
+		case 11:
+		{
+			printf("november - 30 days \n");
+			break;
+		}
+		case 12:
+		{
+			printf("december - 31 days \n");
+			break;
+		}
+		default:
+			printf("invalid");
+		}
+	
+
+
+
 
 
 
